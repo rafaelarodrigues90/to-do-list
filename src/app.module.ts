e5604f893ae682eddc1302c2a27c21942bcfa76d
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-08-17 19:30:54 -0300

    feature: create task service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
+import { TaskService } from './tasks/shared/task.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TasksController],
-  providers: [AppService],
+  providers: [AppService, TaskService],
 })
 export class AppModule {}
