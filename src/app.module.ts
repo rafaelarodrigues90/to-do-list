68b3f8c53033373e14fcc2cfbeb71fa4eec00647
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-08-17 19:38:36 -0300

    refactor: add task controller and service and import task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
-import { TaskService } from './tasks/shared/task.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController, TasksController],
-  providers: [AppService, TaskService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TaskService } from './shared/task.service';
 
-@Module({})
+@Module({
+  controllers: [TasksController],
+  providers: [TaskService]
+})
 export class TasksModule {}
