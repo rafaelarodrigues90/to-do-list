a42b58c42222bdee61a06cea1113977bc1a4f003
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-08-17 19:58:41 -0300

    feature: get all tasks

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: Task[] = [
+    {id: 1, description: 'Tarefa 1', completed: false},
+    {id: 2, description: 'Tarefa 2', completed: false},
+    {id: 3, description: 'Tarefa 3', completed: false},
+    {id: 4, description: 'Tarefa 4', completed: false},
+    {id: 5, description: 'Tarefa 5', completed: false},
+    {id: 6, description: 'Tarefa 6', completed: false},
+    {id: 7, description: 'Tarefa 7', completed: false},
+    {id: 8, description: 'Tarefa 8', completed: false},
+    {id: 9, description: 'Tarefa 9', completed: false},
+    {id: 10, description: 'Tarefa 10', completed: false},
+  ]
+
+  getAll() {
+    return this.tasks
+  }
+}
