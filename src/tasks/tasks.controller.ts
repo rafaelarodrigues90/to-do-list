1e872358a078eabf9203ae07b4f2b32e1045e969
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-08-17 20:04:22 -0300

    feature: get all tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TaskService } from './shared/task.service';
+import { Task } from './shared/task';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(
+    private taskService: TaskService
+    ) { }
+  
+  @Get()
+  async getAll() : Promise<Task[]> {
+    return this.taskService.getAll()
+  }
+}
