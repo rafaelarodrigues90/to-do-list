ba5c37bfe3ddf8b62b8694c50fbf2dfc0bf0a345
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-08-17 19:35:04 -0300

    feature: create task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TaskService } from './tasks/shared/task.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController, TasksController],
   providers: [AppService, TaskService],
 })
